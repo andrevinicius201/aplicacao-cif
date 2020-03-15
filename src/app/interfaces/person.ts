290f38c36162ab383d531293fa34a1461586beec
mathLanca <matheus.7910@hotmail.com>
2020-03-15 19:16:17 -0300

    Adicionando login funcionando

---
+++ src/app/interfaces/person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+
+    firstName: string;
+    lastName: string;
+    birthDate: Date;
+    email: string;
+    password: string;
+    sex: string;
+    telephoneNumber: string;
+}
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders,HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Patient } from '../interfaces/patient';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  patientLogin(email: string, password: string) : Observable<any> {
+    const url = `${environment.personBaseUrl}/v1/patient/login`;
+
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');    
+      
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token',
+        'email': email,
+        'password': password
+      })
+    };
+    return this.http.get<Patient>(url, httpOptions);
+  }
+}
