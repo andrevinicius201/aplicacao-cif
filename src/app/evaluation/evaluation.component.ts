ab53c2d770479b51a922cf9ca3fbffd5de1e632e
mathLanca <matheus.7910@hotmail.com>
2020-05-10 12:26:24 -0300

    Adicionando estrutura da pagina de avaliação

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evaluation',
@@ -12,16 +13,23 @@ export class EvaluationComponent implements OnInit {
 
   panelOpenState = false;
   patientCPF: String; 
-  state$: Observable<object>;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
   
-  constructor(private route:Router) {
+  constructor(private route:Router,private _formBuilder: FormBuilder) {
     if(this.route.getCurrentNavigation().extras != "undefined"){
       this.patientCPF = this.route.getCurrentNavigation().extras.state.patientCpf;
     }
   }
 
   ngOnInit(): void {
-    console.log("CPF",this.patientCPF);
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
+
 
 }
